341a9092805717cd850b17d50017c28484f48bfa
Shivam Sharma <91273192+shivamsharma9627@users.noreply.github.com>
2022-07-06 11:00:57 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -15,6 +15,6 @@ def classify_image():
     return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    print("Starting Python Flask Server For Image Classification")
     util.load_saved_artifacts()
     app.run(debug=True)
